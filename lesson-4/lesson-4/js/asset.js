db9ff9136d184b07a7af929be1152a2fcd35c1b0
Aleksey <pavlyutenkovaleksey@yandex.ru>
2021-07-07 22:32:07 +0500

    Закончил теорию 3-й урок

---
+++ lesson-4/lesson-4/js/asset.js
@@ -0,0 +1,73 @@
+function openConf(text){
+let x = confirm('Это была функция')
+console.log(x + ' ' + text)
+
+}
+
+// openConf('Привет функция!')
+
+/*возращение результата после return функция остановиться*/
+function numbers(){
+	let xx = 55
+	let y = 35
+	return xx + y
+}
+
+
+console.log(numbers())
+// console.log(xx) //локальная переменная функции,ее не видно вне функции
+
+function calc(x,y){
+	return x * y
+}
+
+console.log(calc(55,66))
+console.log(calc(575,676))
+console.log(calc(585,686))
+console.log(calc(595,696))
+
+
+// эту функцию нельзя вызвать до объявления,а другие можно
+let summ = function(r,u){
+	let res = r + u + ' -Это просто текст'
+	return res
+}
+
+console.log('summ:',summ(55,77))
+
+
+let arrow = (x,z) => {
+	let step = (x + z)**4
+	return step
+}
+
+console.log('стрелочная функция',arrow(66,66))
+
+let str = "Длина текста: lenght"
+console.log('str-lenght',str.length)
+console.log('str-toUpperCase',str.toUpperCase())
+console.log('str-toLoverCase',str.toLowerCase())
+
+let numb = 55.789
+let num_str = '55.789'
+console.log('Math.round:',Math.round(numb))
+console.log('parseInt:',parseInt(num_str))
+console.log('parseFloat:',parseFloat(num_str))
+
+
+const btn = document.querySelector('input[type="button"]')
+const input = document.querySelector('input[type="text"]')
+
+btn.addEventListener('click', inputAdgvard)
+
+
+function inputAdgvard(){
+let value = input.value
+if(value === '' && value === null && (typeof (value) === 'number')){
+	alert('Ввод неправильный')
+} else {
+	alert('Все гуд')
+	console.log(value)
+}
+
+}
